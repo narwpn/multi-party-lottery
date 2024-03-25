ca5f337c511b28098934e729cd685ee2df84b851
Narawit Sariddeechaikul <narawitpun@gmail.com>
2024-03-26 01:55:25 +0700

    improve compile script

--- compile.ps1
+++ compile.ps1
@@ -5,6 +5,11 @@ npx solcjs .\src\contracts\Lottery.sol --abi --bin
 Rename-Item -Path ".\src_contracts_Lottery_sol_Lottery.abi" -NewName "LotteryAbi.json"
 Rename-Item -Path ".\src_contracts_Lottery_sol_Lottery.bin" -NewName "LotteryBytecode.bin"
 
+# Create the build directory if it doesn't exist
+if (-not (Test-Path ".\build")) {
+    New-Item -ItemType Directory -Path ".\build"
+}
+
 # Move the renamed files to the build directory
 Move-Item -Path ".\LotteryAbi.json" -Destination ".\build" -Force
 Move-Item -Path ".\LotteryBytecode.bin" -Destination ".\build" -Force
